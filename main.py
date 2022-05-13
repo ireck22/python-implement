7e4231f06e94559f2f4852e1abcd1822d11b12a6
ireck22 <dragon9029@gmail.com>
2022-05-13 22:05:57 +0800

    拿pchome商品初步完成

---
+++ main.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+import sys
+import json
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import pandas as pd
+import numpy as np
+import codecs
+import re
+import ssl
+import time
+import ruten
+import pchome
+import momo
+
+key = input("請輸入要找的商品:")
+finsih_result = []
+
+# =======爬pchome的商品 start==============
+result_pchome = pchome.get_prods(key)
+if len(result_pchome) > 1:
+    finsih_result.append(result_pchome)
+# =======爬pchome的商品 end==============
+
+# =======爬momo的商品 start==============
+result_pchome = momo.get_prods(key)
+if len(result_pchome) > 1:
+    finsih_result.append(result_pchome)
+# =======爬momo的商品 end==============
+
+print(finsih_result)
+sys.exit()
+
+# sorted(result3,key = lambda f: f[0],reverse = True)
+# key2=key.split(' ')
+# length=len(key2)
+# if length>0:
+#     key=key.replace(' ','+')
+
+# url="https://www.ruten.com.tw/find/?q="+key
+
+# options = Options()
+# options.add_argument("--disable-notifications")
+# driver = webdriver.Chrome('./chromedriver', chrome_options=options)    #讀取已經載好的chromedriver
+
+# driver.maximize_window()
+driver = webdriver.Chrome(
+    ChromeDriverManager().install())  # 自動抓取最新的chromedriver
+driver.get(url)
+
+# ----------------------滾動卷軸到最下面 start----------------------
+for i in range(1, 5):
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+time.sleep(1)
+# ----------------------滾動卷軸到最下面 end----------------------
+
+d = driver.page_source  # 把動態爬取的程式碼放到d
+driver.quit()  # 關閉瀏覽器
+
+
+soup = BeautifulSoup(d)
+# print(soup)
+content = soup.find_all('div')
+i = 1
+for row in content:
+    # title=row.find('div',class_="rt-goods-list-item-name")
+    # data=row.find('div',class_='spotlight-section-body').text
+    data = row.find(
+        'div', attrs={"class": ["search-result-container", "top-part"]}).text
+    # data_a=data.find('a')['href']
+    # data_txt=data_a.find('span').text
+    # result=data.split(' ')
+    print(data)
+    break
+    print(i)
+    i += 1
+    # if i==20:
+    #     break
+
+# ========抓兩頁的資料 start===========================
+# for i in range(1, 3):
+#     res = r.get("https://www.ptt.cc/bbs/Gossiping/index"+str(i)+".html")
+#     soup = BeautifulSoup(res.text, 'lxml')
+
+#     result = soup.find_all('div')                   # 先拿全部的div
+#     main_url = 'https://www.ptt.cc/'                # 網址前頭設定
+#     finish = []                                     # 放最後的結果
+
+#     for row in result:
+#         if row.find('div', class_='title'):          # div的class是title再進去
+#             title = row.find('div', class_='title')  # 先篩選title
+#             if title.find('a'):
+#                 title2 = title.find('a').text        # 在進a裡拿文字
+#                 href = title.find('a')['href']       # 拿href
+#                 url = main_url+href                  # 還原網址
+
+#                 # ======== 把文章裡的內文拿出來 start================
+#                 res2 = r.get(url)
+#                 soup2 = BeautifulSoup(res2.text, 'lxml')
+#                 content = soup2.find('div', id='main-content').text
+#                 content2 = content.split("--")[0]  # 不拿回應
+#                 # ======== 把文章裡的內文拿出來 end================
+
+#                 finish.append([title2, url, content2])  # 新增到陣列
+# # ========抓兩頁的資料 start===========================
+
+# # ========輸出csv start===============================
+# rs = pd.DataFrame(finish, columns=['title', 'url', 'content'])
+# rs.to_csv('content.csv')
+# print("end")
+# # ========輸出csv start===============================
---
+++ pchome.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+import sys
+import json
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import pandas as pd
+import numpy as np
+import time
+import urllib.parse
+
+
+def get_prods(key):
+
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'
+    }
+
+    # ==============空格處理 start=========
+    key2 = key.split(' ')
+    length = len(key2)
+    if length > 0:
+        key = urllib.parse.quote(key)
+    # ==============空格處理 start=========
+
+    url = "https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=" + \
+        key+"&page=1&sort=sale/dc"
+
+    # 靜態爬取
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'lxml')
+    content = soup.find('p').text
+    r2 = eval(content)  # 把字串型list轉成list
+    length = len(r2)
+
+    if length < 0:
+        return "沒有此商品"
+
+    # 放商品名稱和和價格
+    result = {"平台:": "PCHOME",
+              "商品名稱": r2['prods'][0]['name'],
+              "價格": r2['prods'][0]['price']
+              }
+
+    return result
