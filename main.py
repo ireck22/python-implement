8565b918369fff9c83252c156f1b07fc7c5bc88c
ireck22 <dragon9029@gmail.com>
2022-05-15 22:32:07 +0800

    主程式和momo、pchome調整

--- main.py
+++ main.py
@@ -18,26 +18,24 @@ import momo
 key = input("請輸入要找的商品:")
 finsih_result = []
 
-# =======爬pchome的商品 start==============
+# =======爬momo的商品 start===============
+result_momo = momo.get_prods(key)
+if len(result_momo) > 1:
+    finsih_result.append(result_momo)
+# =======爬momo的商品 end=================
+
+# =======爬pchome的商品 start=============
 result_pchome = pchome.get_prods(key)
 if len(result_pchome) > 1:
     finsih_result.append(result_pchome)
-# =======爬pchome的商品 end==============
-
-# =======爬momo的商品 start==============
-result_momo = momo.get_prods(key)
-# print(result_momo)
-# sys.exit()
-if len(result_momo) > 1:
-    finsih_result.append(result_momo)
-# =======爬momo的商品 end==============
+# =======爬pchome的商品 end===============
 
 # print(finsih_result)
-# for i in finsih_result:
-#     print(i)
-df=pd.DataFrame(finsih_result)
+for i in finsih_result:
+    print(i)
+df = pd.DataFrame(finsih_result)
 # finish=df.sort_values(by='價格') #價格小到大
-df.to_csv("ss.csv")
+df.to_csv("比價.csv")
 print("end")
 sys.exit()
 
--- pchome.py
+++ pchome.py
@@ -17,12 +17,12 @@ def get_prods(key):
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'
     }
 
-    # ==============空格處理 start=========
+    # ==============空格處理 start==================
     key2 = key.split(' ')
     length = len(key2)
     if length > 0:
         key = urllib.parse.quote(key)
-    # ==============空格處理 start=========
+    # ==============空格處理 end=====================
 
     url = "https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=" + \
         key+"&page=1&sort=sale/dc"
@@ -31,17 +31,23 @@ def get_prods(key):
     res = requests.get(url, headers=headers)
     soup = BeautifulSoup(res.text, 'lxml')
     content = soup.find('p').text
-    r2 = eval(content)  # 把字串型list轉成list
-    length = len(r2)
+    content2 = eval(content)  # 把字串型list轉成list
+    length = len(content2)
 
-    if length < 0:
+    # 沒有資料的話就會退出
+    if length == 0:
         return "沒有此商品"
+
+    # ===========整理資料拿出金額最小的 start===========
+    df = pd.DataFrame(content2['prods'])
+    content3 = df.min().to_dict()  # 找出最小的價格的商品資料轉字典
+    # ===========整理資料拿出金額最小的 end=============
 
     # 放商品名稱和和價格
     result = {
         "平台:": "PCHOME",
-        "商品名稱": r2['prods'][0]['name'],
-        "價格": r2['prods'][0]['price']
+        "商品名稱": content3['name'],
+        "價格": content3['price']
     }
 
     return result
